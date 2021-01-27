8933e13a01484d41ca7c7381c75ae26fa1d68762
Nathan suk <natthansuk@gmail.com>
2021-01-27 12:46:16 +0100

    Added missing files

---
+++ src/Controller/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class TestController extends AbstractController{
+
+    /**
+     * @Route("/", name="direbonjour")
+     */
+    public function sayHello(): Response
+    {
+        $string = 'hhudhus';
+        return $this->render('index.html.twig', [
+            'maphrase' => $string
+        ]);
+    }
+}
