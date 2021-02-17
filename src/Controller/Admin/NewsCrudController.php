d8aa64c1af97df85b9e8a482554898db3c38b341
Nathan suk <natthansuk@gmail.com>
2021-02-17 19:53:08 +0100

    Added webpack-encore-bundle

--- src/Controller/Admin/NewsCrudController.php
+++ src/Controller/Admin/NewsCrudController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Admin;
 use App\Entity\News;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
